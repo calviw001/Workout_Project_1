068e316de9f9746cd8618d9bcd357bbe3e6c3200
Calvin Wong <calviw8@uci.edu>
2024-01-25 21:10:46 -0800

    Created write_into_file(cypher_list, file_name).
    This new function will take the cypher list and another file. It will then write each inverted sentence/line from the list into the file in reverse order.

--- wp1.py
+++ wp1.py
@@ -17,12 +17,23 @@ def read_file(file_name):
         for line in text_file:
             get_sentence = line.strip()
             cypher_list.append(invert_sentence(get_sentence))
-    print(cypher_list)
+    # print(cypher_list)
     return cypher_list
     
 
+def write_into_file(cypher_list, file_name):
+    current_directory = Path.cwd()
+    file_path = current_directory / file_name
+    with file_path.open("w") as text_file:
+        for i in range((len(cypher_list)-1), -1, -1):
+            text_file.write(f"{cypher_list[i]}\n")
+    
+
 def main():
-    a_file = "oldtext.txt"
+    b_file = "oldtext.txt"
+    a_file = "newtext.txt"
+
     cypher_list = read_file(a_file)
+    write_into_file(cypher_list, b_file)
 
 main()
