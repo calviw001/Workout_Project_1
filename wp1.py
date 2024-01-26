fba2b73e703ec2c532deaea7df6ad322c70adfab
Calvin Wong <calviw8@uci.edu>
2024-01-25 20:18:29 -0800

    Updated wp1.py
    Created a function invert_sentence(sentence), which takes in a sentence and inverts the arrangement of characters. I also made a text file called oldtext.txt to test my code.

--- wp1.py
+++ wp1.py
@@ -1,7 +1,18 @@
+import sys
+
+
+def invert_sentence(sentence):
+    if len(sentence) == 1:
+        return sentence
+    else:
+        return sentence[-1] + invert_sentence(sentence[0:-1])
+
 def read_file(file_name):
     pass
+    
 
 def main():
-    pass
+    a_file = "oldtext.txt"
+    read_file(a_file)
 
 main()
